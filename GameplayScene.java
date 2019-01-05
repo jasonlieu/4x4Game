64975f8615422c8917494139c818d3767d1266ab
jasonlieu <jasonlieuthefirst@gmail.com>
2019-01-05 03:52:24 -0800

    reset can be used after win

--- GameplayScene.java
+++ GameplayScene.java
@@ -52,73 +52,40 @@ public class GameplayScene implements Scene{
 
     @Override
     public void receiveTouch(MotionEvent event){
-        float UpX = 0, UpY = 0, Xdif, Ydif;
-        if(!win) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    DownX = event.getX();
-                    DownY = event.getY();
-                    break;
-                case MotionEvent.ACTION_UP:
-                    UpX = event.getX();
-                    UpY = event.getY();
-                    Ydif = DownY - UpY;
-                    Xdif = UpX - DownX;
-                    if (Math.abs(Xdif) > Math.abs(Ydif)) {
-                        if (Math.abs(Xdif) > SWIPE_THRESHOLD) {
-                            if (DownX > WIDTH * 0.22222 && DownX < WIDTH * 0.77774 && DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
-                                if (Xdif > 0) {
-                                    if (DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + WIDTH * 0.13888) {
-                                        board.swipeRight(0);
-                                    } else if (DownY > HEIGHT * 0.5 + WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 2 * WIDTH * 0.13888) {
-                                        board.swipeRight(1);
-                                    } else if (DownY > HEIGHT * 0.5 + 2 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 3 * WIDTH * 0.13888) {
-                                        board.swipeRight(2);
-                                    } else if (DownY > HEIGHT * 0.5 + 3 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
-                                        board.swipeRight(3);
-                                    }
-                                } else {
-                                    if (DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + WIDTH * 0.13888) {
-                                        board.swipeLeft(0);
-                                    } else if (DownY > HEIGHT * 0.5 + WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 2 * WIDTH * 0.13888) {
-                                        board.swipeLeft(1);
-                                    } else if (DownY > HEIGHT * 0.5 + 2 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 3 * WIDTH * 0.13888) {
-                                        board.swipeLeft(2);
-                                    } else if (DownY > HEIGHT * 0.5 + 3 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
-                                        board.swipeLeft(3);
-                                    }
+        float UpX, UpY, Xdif, Ydif;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                DownX = event.getX();
+                DownY = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                UpX = event.getX();
+                UpY = event.getY();
+                Ydif = DownY - UpY;
+                Xdif = UpX - DownX;
+                if (!win && Math.abs(Xdif) > Math.abs(Ydif)) {
+                    if (Math.abs(Xdif) > SWIPE_THRESHOLD) {
+                        if (DownX > WIDTH * 0.22222 && DownX < WIDTH * 0.77774 && DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
+                            if (Xdif > 0) {
+                                if (DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + WIDTH * 0.13888) {
+                                    board.swipeRight(0);
+                                } else if (DownY > HEIGHT * 0.5 + WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 2 * WIDTH * 0.13888) {
+                                    board.swipeRight(1);
+                                } else if (DownY > HEIGHT * 0.5 + 2 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 3 * WIDTH * 0.13888) {
+                                    board.swipeRight(2);
+                                } else if (DownY > HEIGHT * 0.5 + 3 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
+                                    board.swipeRight(3);
                                 }
-                                board.startGame();
-                                if (board.checkWin()) {
-                                    board.stopGame();
-                                    win = true;
-                                }
-                            }
-                        }
-                    } else if (Math.abs(Ydif) > SWIPE_THRESHOLD) {
-                        if (DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
-                            if (Ydif > 0) {
-                                if (DownX > WIDTH * 0.2222 && DownX < WIDTH * 0.3611) {
-                                    board.swipeUp(0);
-                                } else if (DownX > WIDTH * 0.3611 && DownX < WIDTH * 0.49998) {
-                                    board.swipeUp(1);
-                                } else if (DownX > WIDTH * 0.49998 && DownX < WIDTH * 0.63886) {
-                                    board.swipeUp(2);
-                                } else if (DownX > WIDTH * 0.63886 && DownX < WIDTH * 0.77774) {
-                                    board.swipeUp(3);
-                                }
-                                board.startGame();
                             } else {
-                                if (DownX > WIDTH * 0.2222 && DownX < WIDTH * 0.3611) {
-                                    board.swipeDown(0);
-                                } else if (DownX > WIDTH * 0.3611 && DownX < WIDTH * 0.49998) {
-                                    board.swipeDown(1);
-                                } else if (DownX > WIDTH * 0.49998 && DownX < WIDTH * 0.63886) {
-                                    board.swipeDown(2);
-                                } else if (DownX > WIDTH * 0.63886 && DownX < WIDTH * 0.77774) {
-                                    board.swipeDown(3);
+                                if (DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + WIDTH * 0.13888) {
+                                    board.swipeLeft(0);
+                                } else if (DownY > HEIGHT * 0.5 + WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 2 * WIDTH * 0.13888) {
+                                    board.swipeLeft(1);
+                                } else if (DownY > HEIGHT * 0.5 + 2 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 3 * WIDTH * 0.13888) {
+                                    board.swipeLeft(2);
+                                } else if (DownY > HEIGHT * 0.5 + 3 * WIDTH * 0.13888 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
+                                    board.swipeLeft(3);
                                 }
-                                board.startGame();
                             }
                             board.startGame();
                             if (board.checkWin()) {
@@ -126,15 +93,48 @@ public class GameplayScene implements Scene{
                                 win = true;
                             }
                         }
-                    } else if( //reset button
-                            (DownX >= (WIDTH * 0.73) && DownX <= (WIDTH * 0.99)) && //check in resetBounds
-                            (DownY >= (HEIGHT * 0.02) && DownY <= (HEIGHT * 0.06)) &&
-                            (UpX >= (WIDTH * 0.73) && UpX <= (WIDTH * 0.99)) &&
-                            (UpY >= (HEIGHT * 0.02) && UpY <= (HEIGHT * 0.06))
-                            ){
-                                board = new GameBoard(context);
                     }
-            }
+                } else if (!win && Math.abs(Ydif) > SWIPE_THRESHOLD) {
+                    if (DownY > HEIGHT * 0.5 && DownY < HEIGHT * 0.5 + 4 * WIDTH * 0.13888) {
+                        if (Ydif > 0) {
+                            if (DownX > WIDTH * 0.2222 && DownX < WIDTH * 0.3611) {
+                                board.swipeUp(0);
+                            } else if (DownX > WIDTH * 0.3611 && DownX < WIDTH * 0.49998) {
+                                board.swipeUp(1);
+                            } else if (DownX > WIDTH * 0.49998 && DownX < WIDTH * 0.63886) {
+                                board.swipeUp(2);
+                            } else if (DownX > WIDTH * 0.63886 && DownX < WIDTH * 0.77774) {
+                                board.swipeUp(3);
+                            }
+                            board.startGame();
+                        } else {
+                            if (DownX > WIDTH * 0.2222 && DownX < WIDTH * 0.3611) {
+                                board.swipeDown(0);
+                            } else if (DownX > WIDTH * 0.3611 && DownX < WIDTH * 0.49998) {
+                                board.swipeDown(1);
+                            } else if (DownX > WIDTH * 0.49998 && DownX < WIDTH * 0.63886) {
+                                board.swipeDown(2);
+                            } else if (DownX > WIDTH * 0.63886 && DownX < WIDTH * 0.77774) {
+                                board.swipeDown(3);
+                            }
+                            board.startGame();
+                        }
+                        board.startGame();
+                        if (board.checkWin()) {
+                            board.stopGame();
+                            win = true;
+                        }
+                    }
+                }
+                if( //reset button
+                    (DownX >= (WIDTH * 0.73) && DownX <= (WIDTH * 0.99)) && //check in resetBounds
+                    (DownY >= (HEIGHT * 0.02) && DownY <= (HEIGHT * 0.06)) &&
+                    (UpX >= (WIDTH * 0.73) && UpX <= (WIDTH * 0.99)) &&
+                    (UpY >= (HEIGHT * 0.02) && UpY <= (HEIGHT * 0.06))
+                    ){
+                        board = new GameBoard(context);
+                        win = false;
+                }
         }
     }
 
